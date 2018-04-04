555be8fad3fea67e27f65036d534f88ebc6ab05b
Mary-Kate James <marykatj@gmail.com>
2018-04-04 15:06:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    return i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+    array.push(i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`)
   }
+  return array;
 }
